ddc9e8ee69cc675bc5763ae6c14a5e36af6c0db2
MichaelYelich <myelich@hawk.iit.edu>
2022-11-05 17:35:30 -0500

    Initial working code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function init(){
 //add your javascrip between these two lines of code
- 
-
-
-
-
-
-
-
+    function myEventFunction() {
+		var text = document.getElementById('entryinput').value;
+		alert('Michael Yelich: ' + text);
+		document.getElementById('textoutput').innerHTML = text;
+	}
+	var entrybutton = document.getElementById('entrybutton')
+	entrybutton.addEventListener('click', myEventFunction);
+    
+}
 window.addEventListener('load', init);
